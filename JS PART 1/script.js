60037f662d1719e65c696a90ddfc44d3c2a12a16
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2024-09-15 16:50:36 +0300

    Js Declaring Variables and assign them values

--- JS PART 1/script.js
+++ JS PART 1/script.js
@@ -1,1 +1,15 @@
+let firstName = "Valentin";
+console.log(firstName);
+
+// den epitrepetai to onoma ths metavlhths na arxizei ne gramma kefalaio kai leksh-kleidh
+
+// Declare variables called country, continent and population 
+// and assign their values according to your own country (population in millions).
+
+let country, continent , population;
+country = "Albania";
+continent = "Europe";
+population = "3.1 milion";
+console.log(country, continent, population);
+
 
