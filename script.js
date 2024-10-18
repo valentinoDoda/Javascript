b1e447018e317b6942ae495278f8a3762b533706
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2024-10-18 13:17:51 +0300

    Copy Object (Not Deep Copy)

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-'use strict';
+"use strict";
+const person = {
+  firstName: "Name",
+  lastName: "Lastname",
+  age: 10,
+  print: function () {
+    console.log(
+      `${this.firstName} , ${this.lastName}, is ${this.age} years old`
+    );
+  },
+};
+const valentin = Object.assign({}, person);
+[valentin.firstName, valentin.lastName, valentin.age] = [
+  "Valentin",
+  "Doda",
+  21,
+];
+person.print();
+valentin.print();
