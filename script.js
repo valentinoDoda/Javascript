f911d2297786e79eb30896918553c71706aaf3d4
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2024-10-20 01:34:18 +0300

    Logical Assigment Exercise

--- script.js
+++ script.js
@@ -494,3 +494,23 @@ and log a string to the console in this format: "${title}" provides no data abou
 for(let i = 0 ; i < books.length; i++){
   books[i].onlineContent ?? console.log(`${books[i].title}" provides no data about its online content.`);  
 }
+
+
+/* 1.
+Some of the book objects from the books array are missing the edition property.
+ Loop over the books array, and assign this property 
+with a number 1 (if it doesn't already exist). Use logical assignment operators.*/
+
+for(let i = 0 ; i < books.length; i++){
+  books[i].edition ??= 1;   
+}
+
+/*2. 
+Some of the book objects from the books array have the highlighted property,
+ which by default is set to true. Iterate over the books array, and if the thirdParty.
+ goodreads.rating property is less than 4.2, reassign it with false.
+
+Use the &&= operator (tip: you may also need the ! operator)*/
+for(let i = 0 ; i < books.length; i++){
+  books[i].highlighted &&= !(books[i].thirdParty.goodreads.rating < 4.2); 
+}
