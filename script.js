1057310905f506f2eac3f41ac1d06b55371a62dc
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2024-10-19 23:15:13 +0300

    Rest Pattern and Parameters Exercises

--- script.js
+++ script.js
@@ -422,3 +422,28 @@ console.log(bookAuthors);
     console.log(...str);
  }
  spellWord("Barella Nutella Cucurella");
+
+ /* 1.Destructure the keywords property (array) of the first book from the
+  books array into variables called mainKeyword and rest. The first keyword should be assigned to mainKeyword, 
+ and the rest of the keywords should be assigned to the rest variable (it should be an array).*/
+
+ const [mainKeyword , ...rest] = book1.keywords;
+ console.log(mainKeyword, rest);
+
+ /*2.Destructure the second book from the books array into a variable called bookPublisher.
+  The bookPublisher variable should be assigned with the value of the publisher
+  property of the book object.
+   Assign the rest of the properties to the restOfTheBook variable.*/
+const {publisher : bookPublisher, ...restOfTheBook} = book2;
+console.log(bookPublisher, restOfTheBook);
+
+/* 3. Write a function called printBookAuthorsCount that has two parameters called title and authors.
+ The authors parameter should accept any number of arguments. This function should 
+log to the console a string formatted like that: "The book "${title}" has ${authors.length} authors".*/
+
+function printBookAuthorsCount(title, ...authors){
+  console.log(`The book "${title}" has ${authors.length} authors`);
+}
+
+printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne');
+  
