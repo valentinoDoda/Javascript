ef51239424b72f7db6d80970f078554421914feb
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2024-10-20 01:23:53 +0300

    nullish operator exercise

--- script.js
+++ script.js
@@ -487,3 +487,10 @@ console.log(hasExamplesInJava(books[1]));
 for(let i = 0 ; i < books.length; i++){
   books[i].onlineContent && console.log(`${books[i].title}" provides online content.`);
 }
+
+
+/*1. There are objects in the books array that don't have the onlineContent property at all. Loop over the books array, 
+and log a string to the console in this format: "${title}" provides no data about its online content.*/
+for(let i = 0 ; i < books.length; i++){
+  books[i].onlineContent ?? console.log(`${books[i].title}" provides no data about its online content.`);  
+}
