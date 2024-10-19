2ae885c42c7956af8fc40feca762887dabdc9538
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2024-10-20 01:05:49 +0300

    Short Circuiting (&& and ||) Exercises

--- script.js
+++ script.js
@@ -446,4 +446,44 @@ function printBookAuthorsCount(title, ...authors){
 }
 
 printBookAuthorsCount('Algorithms', 'Robert Sedgewick', 'Kevin Wayne');
+
+
+/*1.Some of the book objects have the programmingLanguage property,
+ which specifies what programming language is used in the book, for example
+ 
+ {
+  title: 'Algorithms',
+  author: ['Robert Sedgewick', 'Kevin Wayne'],
+  ...
+  programmingLanguage: 'Java',     // <-- HERE
+}
+Write a function called hasExamplesInJava that takes 
+a book object from the books array as an argument.
+ This function should return true if the book uses Java, 
+ or a string 'no data available' if it uses other language or no programming language at all.
+
+Use short-circuiting.
+
+
+*/
+function hasExamplesInJava(book){
+  return book.programmingLanguage == "Java" || "no data available";
+}
+console.log(hasExamplesInJava(books[0]));
+console.log(hasExamplesInJava(books[1]));
+
+
+/* 2. Some of the book objects have the onlineContent property, which is either true or false.
+ Loop over the books array, and for the books that provide online content, log to the console a string in this format:
+  "${title}" provides online content. Use short-circuiting.
+  {
+  title: 'Operating System Concepts',
+  // ... removed for clarity
+  onlineContent: false,          // <-- HERE
+},
   
+  */
+
+for(let i = 0 ; i < books.length; i++){
+  books[i].onlineContent && console.log(`${books[i].title}" provides online content.`);
+}
