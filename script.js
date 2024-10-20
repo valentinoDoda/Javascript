8fae6599ad0c530a026ef6dd800b81e087be20b2
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2024-10-20 17:19:29 +0300

    Optional Chaining (?.)

--- script.js
+++ script.js
@@ -669,3 +669,13 @@ const newBook2 = {
   author: ['Brian W. Kernighan', 'Dennis M. Ritchie'],
   pages
 }
+
+
+/* 1. Write a function called getFirstKeyword that takes the book object as an argument.
+ This function should return the first keyword from the book's keywords property (array) or undefined (if the keywords property doesn't exist). 
+It shouldn't throw an error. Use optional chaining for that. */
+function getFirstKeyword (book){
+  return book.keywords?.[0]; 
+}
+console.log(getFirstKeyword(books[0]));
+console.log(getFirstKeyword(newBook2));
