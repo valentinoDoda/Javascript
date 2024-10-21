2641c7ac94e0f563e5b2bae3c26446b2995f8b01
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2024-10-21 19:02:33 +0300

    Map part 1 Exercises

--- script.js
+++ script.js
@@ -813,3 +813,37 @@ Delete all items from the uniqueKeywords set.
 uniqueKeywords.clear();
 console.log(uniqueKeywords);
 
+/*
+1.
+Create a new book, but this time, as a Map. Assign it to the bookMap variable. Use this array as initial data:
+[['title', 'Clean Code'], ['author', 'Robert C. Martin']]
+*/
+const bookMap = new Map([['title', 'Clean Code'], ['author', 'Robert C. Martin']]);
+console.log(bookMap);
+
+/*
+2.
+Set a new key in bookMap called pages, and assign it with a number 464.
+
+*/
+bookMap.set(
+  'pages' , 464
+)
+console.log(bookMap);
+
+/*3.
+Get the title and author values from bookMap,
+and log to the console a string formatted like that: "${title} by ${author}".*/
+console.log(`${bookMap.get('title')} by ${bookMap.get('author')} `);
+
+/*4.
+Get the size of bookMap, and log it to the console.
+
+*/
+console.log(bookMap.size);
+
+/*
+5.
+Check if bookMap has the author key. and if so, log "The author of the book is known" to the console.*/
+
+console.log(!bookMap.has('author') || "The author of the book is known");
