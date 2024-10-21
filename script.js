919ad6e1bd3f23efb0538d03987232f97af7e597
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2024-10-21 18:32:44 +0300

    Sets Exercises

--- script.js
+++ script.js
@@ -763,3 +763,53 @@ const entries2 = Object.entries(books[0].thirdParty.goodreads);
 4.
 Log the entries and entries2 variables to the console, and compare them. They should look the same.*/
 console.log(entries, entries2);
+
+
+
+/*
+1. 
+Below is the allKeywords variable, which stores an empty array. 
+Loop over the books array, and fill the allKeywords array with the
+ keywords coming from the keywords property of each book object.
+ The allKeywords array should have just one level (no nested arrays).
+Use whatever loop and methods you want.
+ You can also use the spread syntax.
+  In the end, the allKeywords array should look more or less like this:
+   ['computer science', 'programming', 'algorithms', 'data structures', ...].*/
+const allKeywords = [];
+for(let {keywords} of books){
+  allKeywords.push(...keywords);
+}
+
+
+/*
+2.
+The allKeyword array contains duplicates. Remove them by creating a Set out of that array. 
+Assign the newly created set to the uniqueKeywords variable.*/
+const uniqueKeywords = new Set(allKeywords);
+
+/*
+3.
+Add two more keywords to the uniqueKeywords set, for example, 'coding' and 'science'.*/
+uniqueKeywords.add("coding");
+uniqueKeywords.add("science");
+console.log(uniqueKeywords);
+
+/*
+4.
+Delete 'business' from the uniqueKeywords set.
+
+*/
+uniqueKeywords.delete('business');
+
+/*
+5.
+Create an array out of the uniqueKeywords set, and assign it to the uniqueKeywordsArr variable.*/
+const uniqueKeywordsArr = [...uniqueKeywords];
+
+/*6
+Delete all items from the uniqueKeywords set.
+*/
+uniqueKeywords.clear();
+console.log(uniqueKeywords);
+
