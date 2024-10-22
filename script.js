342e87be3b225e5cec6a41cf05288c9314979c1b
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2024-10-22 18:43:38 +0300

    String Part 1

--- script.js
+++ script.js
@@ -899,3 +899,39 @@ console.log(`An event happened , on avrage every ${90/gameEvents.size} minutes`)
 for (let [minute , event] of gameEvents){
   console.log(`[${minute <= 45 ? 'FIRST HALF' : 'SECOND HALF'}] ${minute}: ${event}`);
 }//4
+
+
+/*
+1.
+Take the ISBN property of the first book from the books array, and log to the 
+console characters at index 6, 4, 9 and 8. Use bracket notation to access individual characters.*/
+console.log(books[0].ISBN[6], books[0].ISBN[4], books[0].ISBN[9], books[0].ISBN[8]);
+
+
+/*
+2.
+Below is the quote variable that stores a string. Find the index of the word 'chess', and log it to the console.
+
+*/
+const quote = 'A computer once beat me at chess, but it was no match for me at kick boxing';
+console.log(quote.indexOf('chess'));
+
+
+/*
+3.
+Extract the word "boxing" from the same quote string, and log it to the console.
+
+*/
+console.log(quote.slice(quote.indexOf('boxing')));
+
+/*4.
+Some authors are noted as "(Contributor)", for example "Julie Sussman (Contributor)". 
+Create a function called isContributor that takes an author's name as an argument, and returns either true 
+(if he's a contributor) of false (if he's not a contributor). The string "(Contributor)" 
+is always the last part of the author's name string.
+*/
+function isContributor(author){
+  return author.indexOf("(Contributor)") != -1 ? true : false; 
+}
+console.log(isContributor('Julie Sussman (Contributor)'));
+
