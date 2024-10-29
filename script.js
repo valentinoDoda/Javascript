1c9e606ee88260a98600fbe8fb4c07a530525f90
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2024-10-29 13:31:51 +0200

    For Each Method

--- script.js
+++ script.js
@@ -73,6 +73,10 @@ const currencies = new Map([
 
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
+movements.forEach((move, index, entireArray ) =>
+
+  console.log("Κίνηση " + (index+1) , (move > 0 ? `Κατάθεση : ${move}` : `Ανάληψη: ${Math.abs(move)}`))
+);
 /////////////////////////////////////////////////
 
 //slice
