1408c29ffa6f6a2b17e0e2028a2e1db6b92b7e60
valentinoDoda <52854095+valentinododa123@users.noreply.github.com>
2024-10-29 12:40:34 +0200

    Starter Code

--- script.js
+++ script.js
@@ -1,67 +1,76 @@
-"use strict";
+'use strict';
 
-/*
-Let's build a simple poll app!
-A poll has a question, an array of options from which people can choose, and an
-array with the number of replies for each option. This data is stored in the starter
-'poll' object below.
-Your tasks:
-1. Create a method called 'registerNewAnswer' on the 'poll' object. The
-method does 2 things:
-1.1. Display a prompt window for the user to input the number of the
-selected option. The prompt should look like this:
-What is your favourite programming language?
-0: JavaScript
-1: Python
-2: Rust
-3: C++
-(Write option number)
-1.2. Based on the input number, update the 'answers' array property. For
-example, if the option is 3, increase the value at position 3 of the array by
-1. Make sure to check if the input is a number and if the number makes
-sense (e.g. answer 52 wouldn't make sense, right?)
-2. Call this method whenever the user clicks the "Answer poll" button.
-3. Create a method 'displayResults' which displays the poll results. The
-method takes a string as an input (called 'type'), which can be either 'string'
-or 'array'. If type is 'array', simply display the results array as it is, using
-console.log(). This should be the default option. If type is 'string', display a
-string like "Poll results are 13, 2, 4, 1".
-4. Run the 'displayResults' method at the end of each
-'registerNewAnswer' method call.
-5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test
-data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll
-object! So what should the this keyword look like in this situation?The Complete JavaScript Course 21
-Test data for bonus:
-§ Data 1: [5, 2, 3]
-§ Data 2: [1, 5, 3, 9, 6, 1]
-Hints: Use many of the tools you learned about in this and the last section 😉
-GOOD LUCK 😀*/
-const poll = {
-  question: "What is your favourite programming language?",
-  options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
-  // This generates [0, 0, 0, 0]. More in the next section!
-  answers: new Array(4).fill(0),
-  registerNewAnswer() {
-    let favProgrammingLang = Number(
-      prompt(`${this.question}\n${this.options.join("\n")}`)
-    );
-    favProgrammingLang > -1 && favProgrammingLang < this.options.length
-      ? this.answers[favProgrammingLang]++
-      : console.log("Wrong Answer");
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
 
-    this.displayResults();
-    this.displayResults("1");
-  },
-  displayResults(type = "array") {
-    console.log(type);
-    type == "array"
-      ? (type = this.answers)
-      : (type = `Poll results are ${this.answers.join(",")}`);
-    console.log(type);
-  },
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
 };
 
-poll.displayResults.call(poll);
-document
-  .querySelector(".poll")
-  .addEventListener("click", poll.registerNewAnswer.bind(poll));
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
